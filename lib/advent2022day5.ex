da0ddd31b48d3c91b6cae905709289fbc5695065
Doug Bruce <queazan@gmail.com>
2023-06-18 19:16:54 -0500

    - brain bad quit now

--- lib/advent2022day5.ex
+++ lib/advent2022day5.ex
@@ -19,18 +19,48 @@ defmodule Advent2022day5 do
     inverted_state = Enum.reverse(start_state)
     {index_row, stack_state} = List.pop_at(inverted_state, 0)
     stack_count = stack_count(index_row)
-    Tuple.duplicate([:empty], stack_count)
+    stacks = List.duplicate([:empty], stack_count)
+
+    load_stacks(stack_state, stacks) |> List.to_tuple()
+    # Tuple.duplicate([:empty], stack_count)
 
   end
 
-  @spec load_stacks(list(String.t()), tuple()) :: tuple()
-  defp load_stacks(src, dst) do
+  @spec load_stacks(list(String.t()), list()) :: tuple()
+  defp load_stacks(stack_state, stacks) do
 
-    {crates_to_load, remaining} = List.pop_at(inverted_stack_state, 0)
+    {crates_to_load, remaining} = List.pop_at(stack_state, 0)
+    stacks = load_crates(crates_to_load, stacks)
+    load_stacks(remaining, stacks)
 
   end
 
-  @spec load_crates(String.t(), tuple())
+  @spec load_crates(String.t(), list(list(String.t()))) :: list()
+  defp load_crates(stack_row, stacks)
+    when stack_row == ""
+    when stacks == []
+  do
+    stacks
+  end
+
+  @spec load_crates(String.t(), list(list(String.t()))) :: list()
+  defp load_crates(stack_row, [first | rest]) do
+    IO.puts("New call to load_crates")
+    IO.puts(stack_row)
+    {current_column, remaining_columns} = String.split_at(stack_row, 4)
+    updated = String.at(current_column, 1) |> String.trim() |> safe_crate_insert(first)
+    load_crates(remaining_columns, rest) |> List.insert_at(0, updated)
+  end
+
+  @spec safe_crate_insert(String.t(), list()) :: list()
+  defp safe_crate_insert("", into) do
+    into
+  end
+
+  @spec safe_crate_insert(String.t(), list()) :: list()
+  defp safe_crate_insert(crate_name, into) do
+    List.insert_at(into, 0, crate_name)
+  end
 
   @spec stack_count(String.t()) :: integer()
   defp stack_count(index_row) do
